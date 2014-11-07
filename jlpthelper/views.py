615a31287135568044921725660a98dcbf4b4bf0
Sergey Demenok <sergey.demenok@gmail.com>
2014-11-07 10:03:13 +0300

    refactor project templates

--- jlpthelper/views.py
+++ jlpthelper/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render_to_response
 
 
 def home(request):
-    return render_to_response('base.html')
+    return render_to_response('index.html')
+
+
+def about(request):
+    return render_to_response('static/about.html')
+
+
+def contact(request):
+    return render_to_response('static/contact.html')
